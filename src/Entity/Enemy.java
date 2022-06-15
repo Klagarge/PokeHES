045329d8f686fde72ee5333ec402b9ed7383fd71
Fastium <79766552+Fastium@users.noreply.github.com>
2022-06-15 18:19:14 +0200

    new font

--- src/Entity/Enemy.java
+++ src/Entity/Enemy.java
@@ -43,7 +43,7 @@ public class Enemy extends Character{
     }
 
     @Override
-    public int getPv() {
+    public int getPv(){
         //the pv can go under 0, but his real pv is 0 (for the player)
         return (pv<0) ? 0 : pv;
     }
