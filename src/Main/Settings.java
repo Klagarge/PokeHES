7273a6b1a3b08e8d92caa52b84e2797b33133549
Rémi Heredero <remi@heredero.ch>
2022-06-17 10:39:29 +0200

    comment settings

--- src/Main/Settings.java
+++ src/Main/Settings.java
@@ -2,12 +2,12 @@ package Main;
 
 public class Settings {
 
-    public static final boolean ANDROID = false;
+    public static final boolean ANDROID = false; 
     public static final int PLAYERS = 1;
     public static final int TIME = 15; // number of minutes for kill all enemy // should be 10
     public static final int RECOVERED = 30; // number of seconds an enemy need for recovered
     public static final int SWITCH_MAP_TIME = 250; // Number of milliseconds the player wait for switch map
 
-    public static final int SIDE = 800;
+    public static final int SIDE = 800; // Do not change // TODO resize possible
     
 }
